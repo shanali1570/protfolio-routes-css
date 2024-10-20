cee1daf2b34b44725028c48ebc752b5ba44334a7
shanali1570 <mrshanali00786@gmail.com>
2024-10-20 23:35:38 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        hello
         <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
